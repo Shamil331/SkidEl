47e8dd435facf0ba77da3981e4c7d8e01e08f3b7
Shamil <shigabiev.shamil@mail.ru>
2023-02-07 08:58:24 +0300

    Добавил модель базы данных с помощью инструмента scaffold

---
+++ Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SkidEl
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Subcategories = new HashSet<Subcategory>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Subcategory> Subcategories { get; set; }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SkidEl
+{
+    public partial class Shop
+    {
+        public Shop()
+        {
+            Discounts = new HashSet<Discount>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string PreviewUrl { get; set; }
+
+        public virtual ICollection<Discount> Discounts { get; set; }
+    }
+}
